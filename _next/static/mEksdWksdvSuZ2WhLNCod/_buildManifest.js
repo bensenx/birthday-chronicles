175d70bceaa735446d85b3538b8323d0ce13a8d5
Bensen <xiebenshuai@gmail.com>
2026-02-11 23:48:52 -0800

    Deploy with correct basePath for GitHub Pages

--- _next/static/mEksdWksdvSuZ2WhLNCod/_buildManifest.js
+++ _next/static/mEksdWksdvSuZ2WhLNCod/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/birthday-chronicles//_next/:path+",
+        "destination": "/birthday-chronicles/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
